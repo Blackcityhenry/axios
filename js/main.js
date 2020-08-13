5d5edaef10576666e3c6ab316cd138a55b1948cd
Henry Yeung <blackcityhenry@gmail.com>
2020-08-13 21:30:22 +0800

    check http on keyup, http body, methods

--- js/main.js
+++ js/main.js
@@ -9,21 +9,47 @@ var sandbox = new Vue(
     ),
     router: new VueRouter(),
     data: {
-      consoleInput: 'https://tmc.allab.net/sessions.php',
+      consoleInput: '',
+      method: 'GET',
+      methods: ['GET', 'POST', 'DELETE', 'OPTIONS', 'HEAD', 'PUT', 'CONNECT', 'PATCH'],
+      body: [],
       resultQuery: ''
     },
     methods: {
+      checkHttp(){
+        return /^https?\:\/\//g.test(this.consoleInput)? this.consoleInput = this.consoleInput.replace(/https?\:\/\//g, '') : null ;
+      },
       process(){
-        var userInput = this.consoleInput;
+        var userInput = 'https://' + this.consoleInput;
         var userOutput = [];
         this.consoleInput = '';
-        axios.get(userInput).then(
+        var httpBody = {};
+
+        if ( this.method == 'GET' && this.body.length){
+          let string = '?';
+          this.body.forEach(current=>{
+            string += current.key + '=' + current.value + '&';
+          })
+          userInput += string;
+        } else if ( this.method == 'POST' ) {
+          this.body.forEach(current=>{
+            httpBody[current.key] = current.value;
+          })
+        }
+
+        axios({
+          method: this.method,
+          url: userInput,
+          body: httpBody
+        }).then(
           res => {
             userOutput = JSON.stringify(res.data).replace(/\[/g, '[<br/>').replace(/\,/g, ',<br/>').replace(/\{/g, '{<br/>').replace(/\}/g, '<br/>}');
-            console.error(userOutput);
             this.resultQuery = userOutput;
           }
         );
+      },
+      addNewBodyRow(){
+        this.body.push({key: '', value: ''})
       }
     }
   }
